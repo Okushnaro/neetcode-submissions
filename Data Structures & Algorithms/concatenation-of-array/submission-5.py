e8b07b9f3072d0297fe3a4f6ce680fcd0f57f6c8
Okushnaro <50107516+Okushnaro@users.noreply.github.com>
2026-08-26 09:30:59 +0200

    Add: concatenation-of-array - submission-5

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        numsSz = len(nums)
+        ans_array=[None] * numsSz * 2
+        for index in range(len(ans_array)):
+            
+            if index < numsSz:
+                calculated_index = index
+            else:
+                calculated_index = index - numsSz
+                
+            ans_array[index] = nums[calculated_index]
+        return ans_array
+        
