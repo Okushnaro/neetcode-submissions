2942efb46bb8c8f6d0d967f646446d3eed4f3933
Okushnaro <50107516+Okushnaro@users.noreply.github.com>
2026-08-26 10:31:37 +0200

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.py
@@ -0,0 +1,32 @@
+#import re
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+        result = 0
+        for oper in operations:
+            # if bool(re.match(r"^[-+]?\d+(\.\d+)?$", oper)):
+            #     record.append(int(oper))
+            #     continue
+            if oper == '+':
+                entryOne = record[-1]
+                entryTwo = record[-2]
+                record.append(entryOne + entryTwo)
+                continue
+            if oper == 'C':
+                record.pop()
+                continue
+            if oper == 'D':
+                entryOne = record[-1]
+                record.append(entryOne * 2)
+                continue
+            record.append(int(oper))
+
+        for i in record:
+            result += i
+
+        return result
+
+
+
+        
+        
